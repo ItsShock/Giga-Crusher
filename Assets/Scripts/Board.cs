001e177e62fab48fbfdf7200eb038a245652e2ba
Piotr Domian <domianpiotr111@gmail.com>
2022-10-28 20:07:45 +0200

    refactoring code

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -100,7 +100,7 @@ public class Board : MonoBehaviour
             {
                 findMatches.CheckBombs();
             }
-            findMatches.currentMatches.Remove(allDots[column, row]);
+            
             GameObject particle = Instantiate(destroyEffect, allDots[column, row].transform.position, Quaternion.identity);
             Destroy(particle, .3f);
             Destroy(allDots[column, row]);
@@ -120,6 +120,7 @@ public class Board : MonoBehaviour
                 }
             }
         }
+        findMatches.currentMatches.Clear();
         StartCoroutine(DecreaseRowCo());
     }
 
