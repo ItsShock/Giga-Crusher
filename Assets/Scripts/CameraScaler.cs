ffaa178be5a6c56eed75d75d9ef3ed17b987ccfc
Piotr Domian <domianpiotr111@gmail.com>
2022-11-07 21:00:18 +0100

    add dynamically resizing camera

---
+++ Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScaler : MonoBehaviour
+{
+    private Board board;
+    public float cameraOffset;
+    public float aspectRatio = 0.625f;
+    public float padding = 2;
+
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+        if(board != null)
+        {
+            RepositionCamera(board.width - 1, board.heigth - 1);
+        }
+    }
+
+    void RepositionCamera(float x, float y)
+    {
+        Vector3 tempPosition = new Vector3(x/2, y/2, cameraOffset);
+        transform.position = tempPosition;
+        if(board.width > board.heigth)
+        {
+            Camera.main.orthographicSize = (board.width / 2 + padding) / aspectRatio;
+        }
+        else
+        {
+            Camera.main.orthographicSize = board.heigth / 2 + padding;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
