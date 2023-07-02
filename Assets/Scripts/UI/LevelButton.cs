727d1b81c7b15981c2498b2046fcb5d3f5299c7f
ItsShock <domianpiotr111@gmail.com>
2023-07-02 21:21:44 +0200

    add saving to binary files, fix some bugs

--- Assets/Scripts/UI/LevelButton.cs
+++ Assets/Scripts/UI/LevelButton.cs
@@ -11,28 +11,53 @@ public class LevelButton : MonoBehaviour
     public Sprite lockedSprite;
     private Image buttonImage;
     private Button myButton;
+    private int starsActive;
 
+    [Header("Level UI")]
     public Image[] stars;
     public Text levelText;
     public int level;
     public GameObject confirmPanel;
 
+    private GameData gameData;
+
     // Start is called before the first frame update
     void Start()
     {
+        gameData = FindObjectOfType<GameData>();
         buttonImage = GetComponent<Image>();
         myButton = GetComponent<Button>();
+        LoadData();
         ActivateStars();
         ShowLevel();
         DecideSprite();
     }
 
-    // COME BACK TO THIS WHEN BINARY FILES WILL DONE
+    void LoadData()
+    {
+        //Is GameData present?
+        if(gameData != null)
+        {
+            //Decide if the level is active
+            if(gameData.saveData.isActive[level -1])
+            {
+                isActive = true;
+            }
+            else
+            {
+                isActive = false;
+            }
+            //Decide how many stars to activate
+            starsActive = gameData.saveData.stars[level - 1];
+        }
+    }
+
     void ActivateStars()
     {
-        for (int i = 0; i < stars.Length; i++)
+        for (int i = 0; i < starsActive; i++)
         {
-            stars[i].enabled = false;
+
+            stars[i].enabled = true;
         }
     }
 
